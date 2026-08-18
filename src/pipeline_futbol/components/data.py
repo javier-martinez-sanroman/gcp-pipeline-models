f0638ae9d833a48692f547deae3956f6da22b642
Javier Martinez <javier.martinez.sanroman@gmail.com>
2026-08-18 12:15:19 +0200

    v1:tf2-cpu.2-14.py310

--- src/pipeline_futbol/components/data.py
+++ src/pipeline_futbol/components/data.py
@@ -6,7 +6,8 @@ from kfp.dsl import Dataset, Output, component
 
 @component(
     # base_image="gcr.io/deeplearning-platform-release/tf2-cpu.2-6:latest",
-    base_image='python:3.11',
+    # base_image='python:3.11',
+    base_image="us-docker.pkg.dev/deeplearning-platform-release/gcr.io/tf2-cpu.2-14.py310:latest",
     packages_to_install=[
         "pandas",
         "google-cloud-bigquery",
--- src/pipeline_futbol/components/evaluation.py
+++ src/pipeline_futbol/components/evaluation.py
@@ -5,7 +5,8 @@ from kfp.dsl import Dataset, Input, Metrics, Model, Output, component
 # ##########################################
 @component(
     # base_image="gcr.io/deeplearning-platform-release/tf2-cpu.2-6:latest",
-    base_image='python:3.11',
+    # base_image='python:3.11',
+    base_image="us-docker.pkg.dev/deeplearning-platform-release/gcr.io/tf2-cpu.2-14.py310:latest",
     packages_to_install=[
         "pandas==1.3.5",
         "joblib==1.1.0",
